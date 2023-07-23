9a313540b5a1e21cdeded43a8c09a9b1d8c5c69c
skybreakbb <willistaylora@gmail.com>
2023-07-22 19:57:32 -0500

    added comment to remind me what to send as json in insomnia

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,6 +31,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  // req.body should be an object with key/value of "tag_name": ""
   try {
     const newTagData = await Tag.create(req.body);
     res.status(200).json(newTagData);
