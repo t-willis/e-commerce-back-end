96bc2fc6ee02b903cc27b873aa6e80f0559ef13d
skybreakbb <willistaylora@gmail.com>
2023-07-22 19:30:18 -0500

    added get routes for products by all and by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,29 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  // req.body should be an object with key/value of "category_name": ""
+  try {
+    const newCategoryData = await Category.create(req.body);
+    res.status(200).json(newCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: { 
+        id: req.params.id 
+      },
+    });
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
