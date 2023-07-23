4feba334686e36edaea5bddb6dbf57bbefd33f50
skybreakbb <willistaylora@gmail.com>
2023-07-22 19:56:42 -0500

    finished writing out routes in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,14 +31,40 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTagData = await Tag.create(req.body);
+    res.status(200).json(newTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+    });
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
