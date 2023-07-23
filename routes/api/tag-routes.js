164b99e0c45e5cf2e85fb394560bf9c5dd4e79d3
skybreakbb <willistaylora@gmail.com>
2023-07-22 19:48:28 -0500

    added route for tag-routes to get by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,20 +16,28 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, ({
+      include: [{ model: Product }],
+    }));
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 
